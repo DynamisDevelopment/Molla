5cf6c642da4b625b8081b0327855a542690fd4d0
Joshua Hall <49533903+DynamisDevelopment@users.noreply.github.com>
2021-07-03 01:14:19 -0400

    Refactoring
    - Navbar components, removed all unneeded links.
    - Fixed routing system.
    - Restructured files for Store components and pages.
    - Added more lazy loading.

--- Frontend/src/routes/index.js
+++ Frontend/src/routes/index.js
@@ -1,32 +1,76 @@
-import React, { useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useEffect, Suspense } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import LoadingOverlay from '../components/features/loading-overlay';
+import LoadingOverlay from '../components/features/loading-overlay'
 
-import { scrollTop } from '../utils';
+import { scrollTop } from '../utils'
+const HomePage = React.lazy(() => import('../components/home/index'))
+const Market = React.lazy(() => import('../components/pages/market'))
+const ProductPages = React.lazy(() => import('./products-route.js'))
+const Blog = React.lazy(() => import('../components/pages/blog'))
+const About = React.lazy(() => import('../components/pages/about'))
+const Contact = React.lazy(() => import('../components/pages/contact'))
 
-const ElementPages = React.lazy( () => import( './elements-route.js' ) );
-const ProductPages = React.lazy( () => import( './products-route.js' ) );
-const ShopPages = React.lazy( () => import( './shop-route.js' ) );
-const BlogPages = React.lazy( () => import( './blogs-route.js' ) );
-const OtherPages = React.lazy( () => import( './others-route.js' ) );
-const HomePage = React.lazy( () => import( './home-route.js' ) );
+const Categories = React.lazy(() => import('../components/pages/categories'))
+const Account = React.lazy(() => import('../components/pages/dashboard'))
+const Wishlist = React.lazy(() => import('../components/pages/wishlist'))
+const Cart = React.lazy(() => import('../components/pages/cart'))
+const Checkout = React.lazy(() => import('../components/pages/checkout'))
 
 export default function AppRoot() {
-    useEffect( () => {
-        scrollTop();
-    }, [] )
+  useEffect(() => scrollTop(), [])
 
-    return (
-        <React.Suspense fallback={ <LoadingOverlay /> }>
-            <Switch>
-                <Route path={ `${process.env.PUBLIC_URL}/elements` } component={ ElementPages } />
-                <Route path={ `${process.env.PUBLIC_URL}/product` } component={ ProductPages } />
-                <Route path={ `${process.env.PUBLIC_URL}/shop` } component={ ShopPages } />
-                <Route path={ `${process.env.PUBLIC_URL}/blog` } component={ BlogPages } />
-                <Route path={ `${process.env.PUBLIC_URL}/pages` } component={ OtherPages } />
-                <Route path={ `${process.env.PUBLIC_URL}/` } component={ HomePage } />
-            </Switch>
-        </React.Suspense>
-    )
+  return (
+    <Suspense fallback={<LoadingOverlay />}>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path={`${process.env.PUBLIC_URL}/product`}
+            component={ProductPages}
+          />
+          <Route
+            exact={true}
+            path={`${process.env.PUBLIC_URL}/`}
+            component={HomePage}
+          />
+          <Route
+            exact={true}
+            path={`${process.env.PUBLIC_URL}/shop`}
+            component={Market}
+          />
+          <Route path={`${process.env.PUBLIC_URL}/blog`} component={Blog} />
+          <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
+          <Route
+            path={`${process.env.PUBLIC_URL}/contact`}
+            component={Contact}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/account`}
+            component={Account}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/wishlist`}
+            component={Wishlist}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/cart`}
+            component={Cart}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/checkout`}
+            component={Checkout}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/categories/:fullwidth`}
+            component={Categories}
+          />
+        </Switch>
+      </BrowserRouter>
+    </Suspense>
+  )
 }
