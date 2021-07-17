4e3dfbcdd97e541c4991485aa4a191360ee12613
Joshua Hall <49533903+DynamisDevelopment@users.noreply.github.com>
2021-07-17 12:19:45 -0400

    Get Posts from DB

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,14 +1,10 @@
 import axios from 'axios'
 
-const API_URL = process.env.PUBLIC_URL
+const url = process.env.REACT_APP_API_URL
 
-// API to get products from mock server
-export const getProducts = function() {
+export const getProducts = () => {
   return axios
-    .get(`${process.env.REACT_APP_API_URL}/product`)
-    .then(res => {
-      console.log(res.data, '===============')
-      return res.data
-    })
+    .get(`${url}/product`)
+    .then(res => res.data)
     .catch(err => err)
 }
